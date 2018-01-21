a4b4980938323df71f3de87c23317133f02b1e33
yanhuan1 <yanhuan1@jd.com>
2018-01-21 20:25:04 +0800

    视图mvc

--- src/main/java/com/yanhuan/yhssm/controller/SalaryController.java
+++ src/main/java/com/yanhuan/yhssm/controller/SalaryController.java
@@ -1,8 +1,10 @@
 package com.yanhuan.yhssm.controller;
 
+import com.yanhuan.yhssm.domain.condition.SalaryCondition;
 import com.yanhuan.yhssm.domain.pojo.Salary;
 import com.yanhuan.yhssm.service.SalaryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
@@ -11,15 +13,19 @@ import javax.annotation.Resource;
  * Created by yanhuan1 on 2018/1/16.
  */
 @Controller
-@RequestMapping("/salary")
+@RequestMapping("salary")
 public class SalaryController {
 
     @Resource
     private SalaryService salaryService;
 
-    @RequestMapping("/insert.do")
-    public void insertSalary(Salary salary){
-        salaryService.insert(salary);
+    @RequestMapping(value = "gotoSalary")
+    public String gotoSalary(Model model,SalaryCondition condition) {
+        Salary salary = salaryService.getSalaryByCondition(condition);
+        if (null != salary) {
+            model.addAttribute("salary", salary);
+        }
+        return "salary/salary";
     }
 
 }
