8aa8a20d27ea448694410561d0e2d6e74573b9d0
yanhuan1 <yanhuan1@jd.com>
2018-01-22 09:36:00 +0800

    引入jquery文件

--- src/main/java/com/yanhuan/yhssm/controller/SalaryController.java
+++ src/main/java/com/yanhuan/yhssm/controller/SalaryController.java
@@ -1,8 +1,11 @@
 package com.yanhuan.yhssm.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.yanhuan.yhssm.domain.condition.SalaryCondition;
 import com.yanhuan.yhssm.domain.pojo.Salary;
 import com.yanhuan.yhssm.service.SalaryService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +20,14 @@ import javax.annotation.Resource;
 @RequestMapping("salary")
 public class SalaryController {
 
+    private static Logger logger = LogManager.getLogger(SalaryController.class);
+
     @Resource
     private SalaryService salaryService;
 
     @RequestMapping(value = "gotoSalary")
     public String gotoSalary(Model model,SalaryCondition condition) {
+        logger.error(JSON.toJSON(condition));
         Salary salary = salaryService.getSalaryByCondition(condition);
         if (null != salary) {
             model.addAttribute("salary", salary);
