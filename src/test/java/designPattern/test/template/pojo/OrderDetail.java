f80e6108d970397fe4f353624674ddc491023a8f
yanhuan1 <yanhuan1@jd.com>
2018-02-11 09:48:22 +0800

    模板模式

---
+++ src/test/java/designPattern/test/template/pojo/OrderDetail.java
@@ -0,0 +1,81 @@
+package designPattern.test.template.pojo;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * 明细
+ */
+public class OrderDetail implements Serializable{
+
+    private Long id;
+
+    private String orderNo;
+
+    private String goodsNo;
+
+    private String goodsName;
+
+    private BigDecimal price;
+
+    private Integer qty;
+
+    private Date productionDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public String getGoodsNo() {
+        return goodsNo;
+    }
+
+    public void setGoodsNo(String goodsNo) {
+        this.goodsNo = goodsNo;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public Date getProductionDate() {
+        return productionDate;
+    }
+
+    public void setProductionDate(Date productionDate) {
+        this.productionDate = productionDate;
+    }
+}
